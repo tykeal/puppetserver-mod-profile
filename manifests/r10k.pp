6cfcf66f3d46a81646e3fbe5d3e5c1aedd955d35
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-06-02 11:03:55 -0700

    Make sure r10k hosts handle proper config file loc
    ::r10k::postrun_command expects the puppet config file to still live in
    /etc/puppet as such we need a little hack to get around that.
    
    Change-Id: I854f23e8fb9d25c46d390ea461d47d735e0839ea
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- manifests/r10k.pp
+++ manifests/r10k.pp
@@ -1,4 +1,12 @@
 class profile::r10k {
   include ::r10k
   include ::r10k::postrun_command
+
+  # get around the fact that ::r10k::postrun_command doesn't seem to
+  # know about the new config path for where to modify puppet.conf
+  file { '/etc/puppet':
+    ensure => link,
+    target => 'puppetlabs/puppet',
+    before => Class['::r10k::postrun_command'],
+  }
 }
