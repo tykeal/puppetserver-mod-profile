43c51a9a2441d50060bdba5c07fd36588bb7faa9
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-06-22 15:51:33 -0700

    Add in ::profile::apache{,::php}
    Add in profiles for apache (httpd) and php
    
    Change-Id: Ieb1755ca5f81198383c00bbce12fb6b0fc648edb
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ manifests/apache.pp
@@ -0,0 +1,19 @@
+class profile::apache {
+  include ::apache
+
+  # for now until we come up with a way to nicely read out all the ports
+  # we listen on, we'll just automatically open 80 & 443
+  firewall { '030 accept incoming HTTP and HTTPS traffic':
+    proto  => 'tcp',
+    port   => ['80', '443'],
+    state  => ['NEW'],
+    action => accept,
+  }
+
+  # since our apache systems are usually hosting DB connected apps,
+  # make sure they can connect to DBs
+  selboolean { 'httpd_can_network_connect_db':
+    persistent => true,
+    value      => on,
+  }
+}
