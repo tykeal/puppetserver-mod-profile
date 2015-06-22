305da43063d552c4edfee44834c418da452a36ff
Josh Farwell <jfarwell@linuxfoundation.org>
2015-06-22 15:36:16 -0700

    Additions to ::profile::mailman3
    The module is not complete yet, and does not manage MTA configuration,
    uwsgi, or firewall port for mailman-web. Included the needed modules to
    configure MTA and uwsgi, created firewall rule. These will be removed
    when the module handles this itself.
    
    Change-Id: I0c6a728d282a3005393f5a338255eb2bade82166
    Signed-off-by: Josh Farwell <jfarwell@linuxfoundation.org>

--- manifests/mailman3.pp
+++ manifests/mailman3.pp
@@ -1,4 +1,25 @@
 class profile::mailman3 {
   include ::mailman3::core
   include ::mailman3::web
+
+  # module is not complete, we are pulling in some
+  # needed modules here in the meantime
+  include ::profile::smtp
+  include ::uwsgi
+
+  $allowed_hosts = hiera('mailman3::web::allowed_hosts')
+  validate_string($allowed_hosts)
+
+  $port   = hiera('mailman3::web::uwsgi_port')
+  validate_array($port)
+
+  # firewall rule for mailman3-web
+
+  firewall { '030 accept incoming uwsgi traffic':
+    proto  => 'tcp',
+    port   => $port,
+    state  => ['NEW'],
+    source => $allowed_hosts,
+    action => accept,
+  }
 }
