474d56c24bb42f0150922fba19f5a3596a256e26
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-06-24 08:09:50 -0700

    Rebuild profile::smtp for new postfix module
    We're switching postfix modules from mjhash -> camptocamp. This builds
    out a more complete smtp solution for us and also now handles opening
    the firewall in the case of a fully fledged mta
    
    NOTE: Do not merge unless also merging the changes to
    puppet/master/Puppetfiles and puppet/hiera
    
    Change-Id: I03224570f7e98abceddcc0c0ec9274808961125b
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- manifests/smtp.pp
+++ manifests/smtp.pp
@@ -1,14 +1,37 @@
 class profile::smtp {
-  $configurerelay = hiera('configurerelay')
-  validate_bool($configurerelay)
+  # force load of ::augeas or a satellite system configuration will fail
+  include ::augeas
 
-  if $configurerelay {
-    # default to configuring mail relay
-    include ::postfix::relay
+  # primary postfix configuration
+  include ::postfix
+
+  # load any extra configurations that need to happen
+  $postfix_configs = hiera_hash('postfix::configs', undef)
+  if is_hash($postfix_configs) {
+    create_resources(::postfix::config, $postfix_configs)
   }
-  else {
-    # fully manage the mail config
-    # this requires a full postfix::config definition in hiera
-    include ::postfix::config
+
+  $postfix_transports = hiera_hash('postfix::transports', undef)
+  if is_hash($postfix_transports) {
+    create_resources(::postfix::transport, $postfix_transports)
+  }
+
+  $postfix_virtuals = hiera_hash('postfix::virtuals', undef)
+  if is_hash($postfix_virtuals) {
+    create_resources(::postfix::virtual, $postfix_virtuals)
+  }
+
+  # handle opening the firewall when we're an mta (we don't need this
+  # for satellite systems)
+  $mta = hiera('postfix::mta', false)
+  if ($mta) {
+    # SMTP operates on port 25 by definition, if the server isn't
+    # operating on that port there is likely an issue
+    firewall { '025 accept all SMTP traffic':
+      proto  => 'tcp',
+      port   => ['25'],
+      state  => ['NEW'],
+      action => accept,
+    }
   }
 }
