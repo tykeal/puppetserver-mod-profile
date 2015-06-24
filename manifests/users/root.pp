d64326a88f7137b58b8975193ba6cb9d35c096da
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-06-24 08:33:36 -0700

    Remove LFCore root mailalias
    No longer need the mailalias setup for root in LFCore as the new postfix
    module handles setting that up as a requirement
    
    Change-Id: I258030260133252c6e5796f6faf1ddc93d94e066
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- manifests/users/root.pp
+++ manifests/users/root.pp
@@ -1,12 +1,4 @@
 class profile::users::root {
-  $roothiera = hiera('root_config')
-  validate_hash($roothiera)
-
-  mailalias { 'root':
-    ensure    => present,
-    recipient => $roothiera['mailalias'],
-  }
-
   file { '/root/.bashrc':
     ensure => present,
     source => 'puppet:///modules/profile/users/dotfiles/root-dot.bashrc',
