d67e990c380f3e9cd2aec7ba5eb514a96cacaab6
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-06-29 15:08:24 -0700

    Allow for configuring dynamic scheds & pools
    Need the ability to specify more than just the default schedules or
    pools on directors and storage nodes.
    
    Change-Id: I48c1b4b800d46923d912ca2beea151a6af0c1a68
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- manifests/bacula/director.pp
+++ manifests/bacula/director.pp
@@ -1,6 +1,11 @@
 class profile::bacula::director {
   include ::bacula::director
 
+  $bacula_schedules = hiera_hash('bacula::schedule', undef)
+  if is_hash($bacula_schedules) {
+    create_resources(::bacula::schedule, $bacula_schedules)
+  }
+
   $port=hiera('bacula::director::port',9101)
   validate_integer($port)
 
