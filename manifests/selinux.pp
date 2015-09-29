caeaef121513d86328b479eaac13eeb545515518
Rob Garth <rgarth@linuxfoundation.org>
2015-09-29 14:22:51 +1000

    Adding daily and weekly selinux audit reports
    Changed script to a puppet template
    
    Change-Id: I1912872e530d9d00d9129fc6a1bcc5acfd5bfde7
    Signed-off-by: Rob Garth <rgarth@linuxfoundation.org>

--- manifests/selinux.pp
+++ manifests/selinux.pp
@@ -1,3 +1,24 @@
 class profile::selinux {
-      include ::selinux::base
+  include ::selinux::base
+
+  $recipients=hiera('selinux::audit_recipients','root@localhost')
+
+  file { '/usr/local/bin/avc-audit-report':
+    ensure  => present,
+    content => template("${module_name}/selinux/avc-audit-report.erb"),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  file { '/etc/cron.d/avc-audit-report.cron':
+    ensure   => present,
+    source   => 'puppet:///modules/profile/selinux/avc-audit-report.cron',
+    owner    => 'root',
+    group    => 'root',
+    mode     => '0644',
+    require  => File['/usr/local/bin/avc-audit-report'],
+    checksum => 'md5',
+  }
+
 }
