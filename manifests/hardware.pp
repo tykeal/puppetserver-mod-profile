3f3532548b74339f95317fca5d6d77e0b0cc20aa
Jordan Conway <jconway@linuxfoundation.org>
2015-10-14 11:30:28 -0400

    Added nagios-plugins-openmanage package to hardware
    Change-Id: I0c55115231b4dce0f0c0e63eaf3bcb1ef937149b
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- manifests/hardware.pp
+++ manifests/hardware.pp
@@ -1,8 +1,15 @@
+# Hardware profile for iron
 class profile::hardware {
 
   if $::manufacturer =~ /(?i-mx:dell.*)/ {
     include ::dell
     include ::dell::openmanage
+    include ::epel
+
+    package { 'nagios-plugins-openmanage':
+      ensure  => installed,
+      require => Yumrepo['epel'],
+    }
   }
 
 }
