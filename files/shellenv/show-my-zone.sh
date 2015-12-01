aaba882c9c2ca1a4f888c26753b0a84ac843cbcc
Rob Garth <rgarth@linuxfoundation.org>
2015-12-01 15:28:55 +1100

    Just making sure each zone has a different color prompt
    Change-Id: I3f088a6bedf649207e1a2ac24faea64bac4b776e
    Signed-off-by: Rob Garth <rgarth@linuxfoundation.org>

--- files/shellenv/show-my-zone.sh
+++ files/shellenv/show-my-zone.sh
@@ -3,16 +3,16 @@ case "${HOSTNAME#*.}" in
     export PS1='[\u@\h\[\e[0;33m\].int\[\e[0;0m\] \W]\$ '
   ;;
   dmz.*)
-    export PS1='[\u@\h\[\e[0;33m\].dmz\[\e[0;0m\] \W]\$ '
+    export PS1='[\u@\h\[\e[0;32m\].dmz\[\e[0;0m\] \W]\$ '
   ;;
   web.*)
-    export PS1='[\u@\h\[\e[0;33m\].web\[\e[0;0m\] \W]\$ '
+    export PS1='[\u@\h\[\e[0;34m\].web\[\e[0;0m\] \W]\$ '
   ;;
   ci.*)
-    export PS1='[\u@\h\[\e[0;33m\].ci\[\e[0;0m\] \W]\$ '
+    export PS1='[\u@\h\[\e[0;36m\].ci\[\e[0;0m\] \W]\$ '
   ;;
   fe.*)
-    export PS1='[\u@\h\[\e[0;33m\].fe\[\e[0;0m\] \W]\$ '
+    export PS1='[\u@\h\[\e[0;35m\].fe\[\e[0;0m\] \W]\$ '
   ;;
 esac
 
