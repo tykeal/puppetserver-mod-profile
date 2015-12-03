d8f085129aa779ba4eaef7b3cfd9cf224eb600ef
Jordan Conway <jconway@linuxfoundation.org>
2015-12-03 16:04:54 -0500

    Added nagios openmanage checks to hardware profile
    nagios check and nrpe definition
    
    Change-Id: I804095e297a2d302948415e984aff031564d2560
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- manifests/hardware.pp
+++ manifests/hardware.pp
@@ -1,6 +1,8 @@
 # Hardware profile for iron
 class profile::hardware {
 
+  $nagios_plugin_dir = hiera('nagios_plugin_dir')
+
   if $::manufacturer =~ /(?i-mx:dell.*)/ {
     include ::dell
     include ::dell::openmanage
@@ -10,6 +12,19 @@ class profile::hardware {
       ensure  => installed,
       require => Yumrepo['epel'],
     }
+
+    nrpe::command {
+      'check_dell_openmanage':
+        # lint:ignore:80chars
+        command => "${nagios_plugin_dir}/check_openmanage -t 60 -e --htmlinfo -b pdisk_cert=all/pdisk_foreign=all/bat_charge=all"
+        # lint:endignore
+    }
+
+    nagios::client::hostservices { "NRPE-DELL-OMSA-${::hostname}":
+      service_description => 'NRPE - Dell Hardware',
+      check_command       => 'check_nrpe!check_openmanage'
+    }
+
   }
 
 }
