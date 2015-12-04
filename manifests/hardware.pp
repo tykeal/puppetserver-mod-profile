88ca9591b843fd8c0ef2a6210a4590764cc73f2f
Jordan Conway <jconway@linuxfoundation.org>
2015-12-04 13:30:14 -0500

    Removed html bits for dell hw nagios check
    Change-Id: Id837bbcc9e18b8b0dbfa0fca5ab5b5bde3103ba4
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- manifests/hardware.pp
+++ manifests/hardware.pp
@@ -23,15 +23,15 @@ class profile::hardware {
     nrpe::command {
       'check_dell_openmanage':
         # lint:ignore:80chars
-        command => "${nagios_plugin_dir}/check_openmanage -t 60 -e --htmlinfo -b pdisk_cert=all/pdisk_foreign=all/bat_charge=all"
+        command => "${nagios_plugin_dir}/check_openmanage -t 60 -b pdisk_cert=all/pdisk_foreign=all/bat_charge=all"
         # lint:endignore
     }
 
     nagios::resource { "NRPE-DELL-OMSA-${::hostname}":
-      resource_type         => 'service',
-      defaultresourcedef    => $defaultserviceconfig,
-      nagiostag             => $nagios_tag,
-      resourcedef           => {
+      resource_type      => 'service',
+      defaultresourcedef => $defaultserviceconfig,
+      nagiostag          => $nagios_tag,
+      resourcedef        => {
         service_description => 'NRPE - Dell Hardware',
         check_command       => 'check_nrpe!check_dell_openmanage',
       },
