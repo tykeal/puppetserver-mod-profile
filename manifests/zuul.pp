6eb6274adc1746316fe3dd659497b3e61bcea1ac
Andrew Grimberg <tykeal@bardicgrove.org>
2016-02-06 12:31:33 -0800

    Add profile::zuul
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

---
+++ manifests/zuul.pp
@@ -0,0 +1,26 @@
+# Class: profile::zuul
+class profile::zuul {
+  include ::zuul
+
+  $zuul_override = hiera('zuul::config_override', undef)
+  if ($zuul_override) {
+    validate_hash($zuul_override)
+
+    if has_key($zuul_override, 'gearman_server') {
+      if has_key($zuul_override['gearman_server'], 'start') {
+        $open_gearmanport = $zuul_override['gearman_server']['start']
+      }
+    } else {
+      $open_gearmanport = true
+    }
+  }
+
+  if $open_gearmanport {
+    firewall { '050 accept all gearman traffic':
+      proto  => 'tcp',
+      dport  => 7430, #zuul doesn't support listening on a different port
+      state  => ['NEW'],
+      action => accept,
+    }
+  }
+}
