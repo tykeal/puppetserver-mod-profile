aabddeac71134cd5868e4b6b1f6fcc398c4f8215
Andrew Grimberg <tykeal@bardicgrove.org>
2016-04-19 07:48:30 -0700

    Transition JJB module
    Switch from the voxpupuli/jenkins_job_builder to tykeal/jjb
    
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/jenkins_job_builder.pp
+++ manifests/jenkins_job_builder.pp
@@ -1,19 +1,22 @@
+# class profile::jenkins_job_builder
 class profile::jenkins_job_builder {
-  include ::jenkins_job_builder
+  include ::jjb
 
-  # Extra ini configs this is not nearly as clean as I would like but
-  # given the current shortcomings of the module it's the best I can do
-  # to make sure we can add extra config bits
-  $extra_configs = hiera('jenkins_job_builder::extra_configs', undef)
-  if ($extra_configs)
-  {
-    validate_hash($extra_configs)
-    $jjb_config_defaults = {
-      path => '/etc/jenkins_jobs/jenkins_jobs.ini',
-    }
+  #include ::jenkins_job_builder
 
-    create_ini_settings($extra_configs, $jjb_config_defaults)
-  }
+  ## Extra ini configs this is not nearly as clean as I would like but
+  ## given the current shortcomings of the module it's the best I can do
+  ## to make sure we can add extra config bits
+  #$extra_configs = hiera('jenkins_job_builder::extra_configs', undef)
+  #if ($extra_configs)
+  #{
+  #  validate_hash($extra_configs)
+  #  $jjb_config_defaults = {
+  #    path => '/etc/jenkins_jobs/jenkins_jobs.ini',
+  #  }
+
+  #  create_ini_settings($extra_configs, $jjb_config_defaults)
+  #}
 
   $jjb_users = hiera('users_jenkins_job_builder', undef)
   if ($jjb_users) {
