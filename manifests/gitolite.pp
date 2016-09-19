a33922d20b22b4ecfc2f80a01ba71642b9839ff0
Andrew Grimberg <tykeal@bardicgrove.org>
2016-09-19 13:45:28 -0700

    Make sure that profile::git gets created before gitolite
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/gitolite.pp
+++ manifests/gitolite.pp
@@ -2,4 +2,8 @@
 class profile::gitolite {
   include ::profile::git
   include ::gitolite
+
+  # Make sure that profile git happens before gitolite
+  Class['::profile::git'] ->
+  Class['::gitolite']
 }
