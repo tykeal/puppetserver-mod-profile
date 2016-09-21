62514cc9a10de60fc2c526f4e2130baaa8da621e
Andrew Grimberg <tykeal@bardicgrove.org>
2016-09-21 12:51:00 -0700

    Enable public git daemon mirror with gitolite
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/gitolite.pp
+++ manifests/gitolite.pp
@@ -3,6 +3,10 @@ class profile::gitolite {
   include ::profile::git
   include ::gitolite
 
+  if hiera('gitolite::public_mirror', false) {
+    include ::profile::git::daemon
+  }
+
   # Make sure that profile git happens before gitolite
   Class['::profile::git'] ->
   Class['::gitolite']
