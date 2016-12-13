fcaf3b0811fc9762b7a4dd4a08155fab376d05be
Andrew Grimberg <tykeal@bardicgrove.org>
2016-12-13 11:17:52 -0800

    Create an nss cert database
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/ldap/client.pp
+++ manifests/ldap/client.pp
@@ -2,4 +2,15 @@
 class profile::ldap::client {
   include ::nsstools
   include ::openldap::client
+
+  # configure the openldap cacert store
+  $tls_cacertdir = hiera('openldap::client::tls_cacertdir',
+    '/etc/openldap/certs')
+
+  # create the nss database
+  $nss_ldap_cacert_pass = hiera('nsstools::ldap_cacert_pass',
+    fqdn_rand_string(50, '', 'OpenLDAP CACert Password'))
+  nsstools::create { $tls_cacertdir:
+    password => $nss_ldap_cacert_pass,
+  }
 }
