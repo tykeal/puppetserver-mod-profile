a2e1b7c142d7ad5a648df1063db2b330b1a5ee3c
Andrew Grimberg <tykeal@bardicgrove.org>
2016-12-13 11:55:49 -0800

    Push cacerts into nss cert store
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/ldap/client.pp
+++ manifests/ldap/client.pp
@@ -13,4 +13,39 @@ class profile::ldap::client {
   nsstools::create { $tls_cacertdir:
     password => $nss_ldap_cacert_pass,
   }
+
+  $cacerts = hiera('ldap::client::cacerts', undef)
+  if (is_hash($cacerts)) {
+    $cacerts.each |String $cacert, Hash $options| {
+      sslmgmt::ca_dh { $cacert:
+        * => $options,
+      }
+
+      # futz around with figuring out the actual cert file that was just created
+      if (has_key($options, 'customstore') and
+          (has_key($options['customstore'], 'certfilename') or
+          has_key($options['customstore'], 'certpath'))) {
+        if has_key($options['customestore'], 'certfilename') {
+          $_certname = $options['customstore']['certfilename']
+        }
+        else
+        {
+          $_certpath = $options['customstore']['certpath']
+          $_certname = "${_certpath}/${cacert}.pem"
+        }
+      }
+      else
+      {
+        include ::sslmgmt::params
+
+        $_certpath = $::sslmgmt::params::pkistore['default']['certpath']
+        $_certname = "${_certpath}/${cacert}.pem"
+      }
+
+      nsstools::add_cert { $cacert:
+        certdir => $tls_cacertdir,
+        cert    => $_certname,
+      }
+    }
+  }
 }
