52364b544bd3a2a78f3b43baee77a50c823e8ff8
Andrew Grimberg <tykeal@bardicgrove.org>
2018-04-19 09:21:30 -0700

    Update CLM profile to support test lab
    The CLM profile was incapable of operating without SSL certificates
    defined. This makes it difficult to operate in a test lab that does not
    have any valid certs present.
    
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- manifests/clm.pp
+++ manifests/clm.pp
@@ -1,3 +1,4 @@
+# class profile::clm
 class profile::clm {
   include ::profile::java
   include ::clm
@@ -29,30 +30,79 @@ class profile::clm {
   # nginx setup
   $export_nginx = hiera('clm::export_nginx', false)
   if ($export_nginx) {
-    $ssl_cert_name = hiera('nginx::ssl_cert_name')
-    $ssl_cert_chain = hiera('nginx::ssl_cert_chain')
-    $ssl_dhparam = hiera('nginx::ssl_dhparam')
     $nginx_exporttag = hiera('nginx::exporttag')
     $clm_sitename = hiera('clm::sitename')
 
+    # CLM scans are usually rather small, but some of our projects are getting
+    # large enough that they run into upload limit problems. Let's set a global
+    # default to 100m for CLM and then make it so we can tune it if needed
+    $nginx_uploadlimit = hiera('clm::upload_limit', '100m')
+
+    $location_cfg_append = {
+      'client_max_body_size' => $nginx_uploadlimit,
+    }
+
+    # configure vhost options similar to nexus
+    $vhost_cfg_prepend = {
+      'proxy_send_timeout' => '120',
+      'proxy_buffering'    => 'off',
+      'keepalive_timeout'  => '5 5',
+      'tcp_nodelay'        => 'on',
+    }
+
+    # default hsts to 180 days (SSLLabs recommended)
+    $hsts_age = hiera('nginx::max-age', '15552000')
+
+    $ssl_cert_name = hiera('nginx::ssl_cert_name', undef)
+    $ssl_cert_chain = hiera('nginx::ssl_cert_chain', undef)
+
+    if ($ssl_cert_name and $ssl_cert_chain) {
+      $_ssl_cert = "/etc/pki/tls/certs/${ssl_cert_name}-${ssl_cert_chain}.pem"
+      $_ssl_key = "/etc/pki/tls/private/${ssl_cert_name}.pem"
+      $_ssl = true
+      $_add_header = {
+        'Strict-Transport-Security' => "max-age=${hsts_age}",
+      }
+    } else {
+      $_ssl_cert = undef
+      $_ssl_key = undef
+      $_ssl = false
+      $_add_header = undef
+    }
+
+    # we don't force all of our sites to use more secure dhparam settings
+    # we should, but doing so now would break a lot of stuff!
+    $ssl_dhparam = hiera('nginx::ssl_dhparam', undef)
+    if ($ssl_dhparam) {
+      $_ssl_dhparam = "/etc/pki/tls/certs/${ssl_dhparam}.pem"
+    } else {
+      $_ssl_dhparam = undef
+    }
+
     @@nginx::resource::vhost { "nginx_clm-${clm_sitename}":
-      ensure           => present,
-      server_name      => [[$clm_sitename,],],
-      access_log       => "/var/log/nginx/clm-${clm_sitename}_access.log",
-      error_log        => "/var/log/nginx/clm-${clm_sitename}_error.log",
-      rewrite_to_https => true,
-      ssl              => true,
-      # lint:ignore:80chars
-      ssl_cert         => "/etc/pki/tls/certs/${ssl_cert_name}-${ssl_cert_chain}.pem",
-      # lint:endignore
-      ssl_key          => "/etc/pki/tls/private/${ssl_cert_name}.pem",
-      ssl_dhparam      => "/etc/pki/tls/certs/${ssl_dhparam}.pem",
-      autoindex        => 'off',
-      proxy            => "http://${::fqdn}:${clm_port}",
-      tag              => $nginx_exporttag,
+      ensure              => present,
+      server_name         => [[$clm_sitename,],],
+      access_log          => "/var/log/nginx/clm-${clm_sitename}_access.log",
+      error_log           => "/var/log/nginx/clm-${clm_sitename}_error.log",
+      # flag ipv6 as enabled. This will enable it if possible on the nginx
+      # host side
+      ipv6_enable         => true,
+      ipv6_listen_options => '',
+      location_cfg_append => $location_cfg_append,
+      ssl                 => $_ssl,
+      rewrite_to_https    => $_ssl,
+      ssl_cert            => $_ssl_cert,
+      ssl_key             => $_ssl_key,
+      ssl_dhparam         => $_ssl_dhparam,
+      autoindex           => 'off',
+      vhost_cfg_prepend   => $vhost_cfg_prepend
+      proxy               => "http://${::fqdn}:${clm_port}",
+      # Sonatype suggests setting at least 600 timeout for CLM
+      proxy_read_timeout  => '600',
+      tag                 => $nginx_exporttag,
     }
   }
 
-  Class['::java'] ->
-  Class['::clm']
+  Class['::java']
+  -> Class['::clm']
 }
